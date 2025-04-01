acafc95ba69f5b79f90951aad2dee13921ca7fe2
suresh-offcl <sureshgopikhanishk@gmail.com>
2025-04-01 11:15:09 +0530

    check

---
+++ 50-app-alb/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "81s-khanishk-remote-state"
+    key    = "expense-vpc-dev"
+    region = "us-east-1"
+    dynamodb_table = "81s-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
